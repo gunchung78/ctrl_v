bd6d954a17a73c3a445575d17074d3406a19a82d
geon <gunchung78@gmail.com>
2022-01-26 08:28:30 +0900

    2.0.0 [+ keyboard]

--- control_v.py
+++ control_v.py
@@ -1,4 +1,7 @@
 import pyperclip
+import keyboard
+import time
+
 file_position = input("Enter folder location(txt) or D:\ctrl_v.txt ")
 if file_position == "":
     file_position = "D:\ctrl_v.txt"
@@ -8,16 +11,26 @@ copy_name = []
 lines = f.readlines()
 while True:
     try:
-        temp = input("[ctrl+v]name: ")
+        num = 0
+        while True:
+            num += 1
+            if keyboard.is_pressed(str(num)) & keyboard.is_pressed('ctrl'):
+                temp = int(num)
+                time.sleep(0.1)
+                break
+            if num >= 9:
+                num = 0
+        #temp = input("[ctrl+v]name: ")
         if temp == "":
             raise ValueError
-        temp2 = lines.index( temp + "\n") + 1
+        temp2 = lines.index( str(temp) + "\n") + 1
         for i in range(len(lines)): 
             if lines[temp2 + i] == "\n": break
             copy_name.append(lines[temp2 + i])
-        print("".join(copy_name))
-        pyperclip.copy("".join(copy_name))
-        copy_name.clear()
+        if num != 0:
+            print("".join(copy_name))
+            pyperclip.copy("".join(copy_name))
+            copy_name.clear()
     except ValueError:
         print("x\n")
 f.close()
